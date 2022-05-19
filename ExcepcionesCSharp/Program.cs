4398e8008e1ae3c6acf71896b0b6664b33e010b4
YayoReynoso <105455890+YayoReynoso@users.noreply.github.com>
2022-05-19 13:04:20 -0700

    Proyecto - Uso de excepciones

---
+++ ExcepcionesCSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcepcionesCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Ingresa el numero 1: ");
+                int n1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Ingresa el numero 2: ");
+                int n2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(n1 / n2);
+                Console.ReadKey();
+            }catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
